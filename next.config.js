f06b97c24b04dc8867c1d7e003d994c0e6792473
BOSUNG BAEK <bosung100@actbase.io>
2022-10-06 14:09:31 +0900

    chore: update absolute paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config) {
+    config.resolve.modules.push(__dirname); // 추가
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
